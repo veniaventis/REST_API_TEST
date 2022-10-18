c26e09a93708b605cb5f3bd910c552726a4bf126
d.ventsis <4aba3691@gmail.com>
2022-10-18 15:04:37 +0200

    pom init

---
+++ src/main/java/utils/Specifications.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class Specifications {
+    public static RequestSpe
+}
---
+++ src/test/java/ResApiTestGETPOST.java
@@ -0,0 +1,4 @@
+public class ResApiTestGETPOST {
+    private final String URL = "https://jsonplaceholder.typicode.com.";
+
+}
